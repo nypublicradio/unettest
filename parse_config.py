03316e22d606fb1f7c3a0a63acdaf42e40667aba
Jordan Dashel <jdashel@nypublicradio.org>
2020-04-07 15:30:58 -0400

    docker(-compose) integration

--- parse_config.py
+++ parse_config.py
@@ -14,6 +14,21 @@ def parse_input_config():
         except yaml.YAMLError as e:
             print(e)
 
+def generate_dockercompose(services):
+    """
+    Accept list of services and generate a docker-compose file.
+    """
+    with open('docker-compose.yml', 'w') as f:
+        f.write("version: '3'\n")
+        f.write("services:\n")
+        for service in services:
+            f.write(f'  {service.name}:\n')
+            f.write(f'    build: ./{service.name}\n')
+            f.write(f'    ports:\n')
+            f.write(f'      - "{service.exposed_port}:{service.exposed_port}"\n')
+            f.write(f'    expose:\n')
+            f.write(f'      - {service.exposed_port}')
+
 class Service:
     """
     Representation of a single SERVICE as represented in YAML.
@@ -33,6 +48,7 @@ class Service:
     def __init__(self, name):
         self.name = name
         self.routes = []
+        self.exposed_port = 0
 
     def __str__(self):
         return "Service " + self.name
@@ -44,6 +60,12 @@ class Service:
         r = self.Route(route_config['name'], route_config['route'], route_config['method'], 
                 route_config['status'],  route_config['params'])
         self.routes.append(r)
+
+    def insert_requirements(self, filename):
+        requirements = ['flask']
+        with open(filename, 'w') as f:
+            for requirement in requirements:
+                f.write(requirement + '\n')
 
     def generate_service(self, filename):
         """
@@ -66,17 +88,37 @@ def {r.name}({method_sig}, methods=['{r.method}']):
 
 """)
 
+    def insert_dockerfile(self, filename, exposed_port):
+        with open(filename, 'w') as f:
+            f.write(f"""FROM python:3.7-alpine
+WORKDIR /code
+ENV FLASK_APP main.py
+ENV FLASK_ENV development
+ENV FLASK_RUN_HOST 0.0.0.0
+RUN apk add --no-cache gcc musl-dev linux-headers
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+COPY . .
+CMD ["flask", "run", "-p", "{exposed_port}"]
+""")
 
-if __name__ == '__main__':
-    config = parse_input_config()
-    services = []
-    for service_name in config['services']:
-        s = Service(service_name)
-        for route_ in config['services'][service_name]['routes']:
-            s.load_route(route_)
-        services.append(s)
 
-    for service in services:
-        if not os.path.exists(f'./{service.name}'):
-            os.mkdir(f'./{service.name}')
-        service.generate_service(f'./{service.name}/main.py')
+config = parse_input_config()
+services = []
+exposed_port = 5000
+for service_name in config['services']:
+    s = Service(service_name)
+    for route_ in config['services'][service_name]['routes']:
+        s.load_route(route_)
+    s.exposed_port = exposed_port
+    exposed_port += 1
+    services.append(s)
+
+for service in services:
+    if not os.path.exists(f'./{service.name}'):
+        os.mkdir(f'./{service.name}')
+    service.generate_service(f'./{service.name}/main.py')
+    service.insert_dockerfile(f'./{service.name}/Dockerfile', service.exposed_port)
+    service.insert_requirements(f'./{service.name}/requirements.txt')
+
+generate_dockercompose(services)
