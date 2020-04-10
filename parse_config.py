63082475b1323f71add5bbd56b2777c569120810
Jordan Dashel <jdashel@nypublicradio.org>
2020-04-10 12:37:54 -0400

    test runners

--- parse_config.py
+++ parse_config.py
@@ -3,6 +3,9 @@ import re
 import sys
 import os
 import shutil
+import time
+import requests
+import json
 
 def parse_input_config():
     """
@@ -22,9 +25,9 @@ def generate_dockercompose(services):
     with open('docker-compose.yml', 'w') as f:
         f.write("version: '3'\n")
         f.write("services:\n")
-        for service in services:
-            f.write(f'  {service.name}:\n')
-            f.write(f'    build: ./{service.name}\n')
+        for name, service in services.items():
+            f.write(f'  {name}:\n')
+            f.write(f'    build: ./{name}\n')
             f.write(f'    ports:\n')
             f.write(f'      - "{service.exposed_port}:{service.exposed_port}"\n')
             f.write(f'    expose:\n')
@@ -91,7 +94,16 @@ class Service:
         route_vars = re.compile(r'<(\w*)>')
         with open(filename, 'w') as f:
             f.write('from flask import Flask, request\n')
+            f.write('import inspect\n')
+            f.write('import time\n')
             f.write('app = Flask(__name__)\n')
+            f.write('last_requests = []\n')
+            f.write(f"""
+@app.route('/last_call')
+def last_call():
+    return last_requests[-1]
+
+""")
             for r in self.routes:
                 path_vars = route_vars.findall(r.route_)
                 method_vars = ""
@@ -100,6 +112,8 @@ class Service:
                 f.write(f"""
 @app.route('{r.route_}')
 def {r.name}({method_vars}methods=['{r.method}']):
+    func_name = inspect.currentframe().f_code.co_name
+    last_requests.append({{"test": func_name, "route": "{r.route_}", "time": time.time()}})
     return 'thanx', {r.status}
 
 """)
@@ -120,7 +134,7 @@ CMD ["flask", "run", "-p", "{exposed_port}"]
 
 
 config = parse_input_config()
-services = []
+services = {}
 exposed_port = 5000
 for service_name in config['services']:
     s = Service(service_name)
@@ -129,14 +143,14 @@ for service_name in config['services']:
         s.load_route(route_)
     s.exposed_port = exposed_port
     exposed_port += 1
-    services.append(s)
+    services[service_name] = s
 
-for service in services:
-    if not os.path.exists(f'./{service.name}'):
-        os.mkdir(f'./{service.name}')
-    service.generate_service(f'./{service.name}/main.py')
-    service.insert_dockerfile(f'./{service.name}/Dockerfile', service.exposed_port)
-    service.insert_requirements(f'./{service.name}/requirements.txt')
+for service_name, service in services.items():
+    if not os.path.exists(f'./{service_name}'):
+        os.mkdir(f'./{service_name}')
+    service.generate_service(f'./{service_name}/main.py')
+    service.insert_dockerfile(f'./{service_name}/Dockerfile', service.exposed_port)
+    service.insert_requirements(f'./{service_name}/requirements.txt')
 
 if not os.path.exists('./nginx_server'):
     os.mkdir('./nginx_server')
@@ -145,8 +159,42 @@ if not os.path.exists('./nginx_server/conf'):
 os.system('rm ./nginx_server/conf/*')
 os.system('cp ./nginx/* ./nginx_server/conf')
 with open('./nginx_server/Dockerfile', 'w') as f:
-    f.write("""from openresty/openresty:buster-fat
-""")
+    f.write("""from openresty/openresty:buster-fat""")
 
 
 generate_dockercompose(services)
+
+ready_to_run = input('are you ready to run tests? y/n\n')
+
+if ready_to_run == 'y':
+    os.system('docker-compose up --build -d')
+    time.sleep(3)
+elif ready_to_run == 'n':
+    quit()
+
+for test in config['tests']:
+    print("testing", test)
+    mytest = config['tests'][test]
+    request_type, target = mytest['send'], mytest['target']
+
+    if request_type == 'GET':
+        response = requests.get(f'http://localhost:4999{target}')
+    elif request_type == 'POST':
+        response = None
+
+    expect = mytest['expect']
+    sys_route, expected_results = list(expect.items())[0]
+    system, route_ = sys_route.split('.')
+    sys_under_test = services.get(system)
+    last_req = requests.get(f'http://localhost:{sys_under_test.exposed_port}/last_call')
+    stats = json.loads(last_req.text)
+    target = mytest['target']
+    for varname, varvalue in mytest['vars'].items():
+        # un-interpolate variables
+        target = target.replace(varvalue, f'<{varname}>')
+
+    assert stats['route'] == target
+
+    assert response.status_code == expected_results['return_status']
+
+os.system('docker-compose down')
