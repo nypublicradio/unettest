d1fef8493a25538c6fcb8724de83da8c9edb1199
Jordan Dashel <jdashel@nypublicradio.org>
2020-05-08 16:11:16 -0400

    unettest reveal

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 ################################################################################
 #
-#   NGINXRAY BUILD SCRIP
+#   UNETTEST BUILD SCRIP
 #
 #   builds:
-#	- nginxray
+#	- UNETTEST
 #	- docs
 #
 #   uploads:
-#	- nginxray
+#	- unettest
 #	- docs
 #
 ################################################################################
@@ -17,13 +17,13 @@
 function build_and_deploy_binary {
 	echo '#### FRESH BUILD ####'
 
-	pyinstaller nginxray.py -p ~/.virtualenvs/nginxray/lib/python3.7/site-packages --onefile --name nginxray.mac
+	pyinstaller unettest.py -p ~/.virtualenvs/unettest/lib/python3.7/site-packages --onefile --name unettest.mac
 
-	sha_val=$( shasum -a 256 dist/nginxray.mac | cut -d ' ' -f 1 )
+	sha_val=$( shasum -a 256 dist/unettest.mac | cut -d ' ' -f 1 )
 	echo $sha_val > mac-sha256
 
-	aws s3 cp ./dist/nginxray.mac s3://nginxray/ --acl public-read
-	aws s3 cp ./mac-sha256 s3://nginxray/ --acl public-read
+	aws s3 cp ./dist/unettest.mac s3://unettest/ --acl public-read
+	aws s3 cp ./mac-sha256 s3://unettest/ --acl public-read
 }
 
 function build_and_deploy_docs {
