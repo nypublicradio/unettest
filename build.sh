bf82493f302cef8151342486aa79e11d1cc24c04
Jordan Dashel <jdashel@nypublicradio.org>
2020-07-15 15:53:29 -0400

    rename sha hash

--- build.sh
+++ build.sh
@@ -20,10 +20,10 @@ function build_and_deploy_binary {
 	pyinstaller unettest.py -p ~/.virtualenvs/unettest/lib/python3.7/site-packages --onefile --name unettest.mac
 
 	sha_val=$( shasum -a 256 dist/unettest.mac | cut -d ' ' -f 1 )
-	echo $sha_val > mac-sha256
+	echo $sha_val > unettest.mac-sha256
 
 	aws s3 cp ./dist/unettest.mac s3://unettest/ --acl public-read
-	aws s3 cp ./mac-sha256 s3://unettest/ --acl public-read
+	aws s3 cp ./unettest.mac-sha256 s3://unettest/ --acl public-read
 }
 
 function build_and_deploy_docs {
